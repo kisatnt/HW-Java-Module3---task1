2e4fbe7937277373bfb1d6fa589fe79db46f6e74
Andrei Kiselev <eparis@atlassian.com>
2022-12-22 15:40:16 +0700

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public static int calculate(int price) {
+
+        int cost = price / 20;
+
+
+        return cost;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 20_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+        price = 10_000;
+        miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
